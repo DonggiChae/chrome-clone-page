5642014d590cc6e13ca98641ae65815d6c7bea0e
Dongi <chaedonggi@gmail.com>
2022-03-29 01:46:04 +0900

    rebuilding

--- JS/weather.js
+++ JS/weather.js
@@ -1,23 +1,24 @@
 const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
-const API_KEY = ""; //Write your openweather API key
+const API_KEY = "95f644b9d8186a4cdd91e704642617a6"; //Write your openweather API key
 
 
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    console.log(url);
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-      city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-    });
-  }
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      });
+}
 
 
 function onGeoError() {
-alert("Can't find you. No weather for you.");
+  alert("Can't find you. No weather for you.");
 }
-  
-  navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
